9fa81d94c04c71825f562e066995c94029438aa0
Camila Vildoso <camiliun123@gmail.com>
2024-07-06 14:19:37 -0300

    Initial commit

---
+++ src/projeto/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package projeto.repository;
+
+import projeto.model.Pedido;
+
+public interface ProdutoRepository {
+
+	//CRUD 
+		public void procurarPorNumero(int numero);
+		public void listarTodos();
+		public void atualizar(Pedido produto);
+		public void deletar(int numero);
+	
+}
